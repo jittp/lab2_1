bd04fab5dee252b35de326d1fa824200e09d1806
Jittip <6511130073@mut.ac.th>
2024-07-05 17:33:14 +0700

    commit4

--- manu.php
+++ manu.php
@@ -1,3 +1,6 @@
 <?php 
-echo "<h1My repository<h1>"
- echo "<h1My name is aj yai<h1>"
+echo "<h1My repository<h1>";
+ echo "<h1My name is aj yai<h1>";
+ \echo "<h1My name<h1>";
+
+ ?>
---
+++ readme.php
@@ -0,0 +1,4 @@
+<?php
+    echo <h1>JitTip KangSila</h1> 
+    echo <h1>Embedded</h1>
+?>
